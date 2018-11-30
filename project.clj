a989102720e88ade163b2d6a1bb1d058cd6bf0b1
Alex-Bakic <alexbakic@outlook.com>
2018-11-30 18:48:18 +0000

    Setup project clojure for development

---
+++ project.clj
@@ -0,0 +1,35 @@
+(defproject todomvc-re-frame "0.10.5"
+  :dependencies [[org.clojure/clojure        "1.9.0"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.6"]
+                 [bidi "2.1.4"]]
+
+
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel  "0.5.17"]]
+
+  :hooks [leiningen.cljsbuild]
+
+  :profiles {:dev  {:cljsbuild
+                    {:builds {:client {:compiler {:asset-path           "js"
+                                                  :optimizations        :none
+                                                  :source-map           true
+                                                  :source-map-timestamp true
+                                                  :main                 "sketchy.core"}
+                                       :figwheel {:on-jsload "sketchy.core/main"}}}}}
+
+             :prod {:cljsbuild
+                    {:builds {:client {:compiler {:optimizations :advanced
+                                                  :elide-asserts true
+                                                  :pretty-print  false}}}}}}
+
+  :figwheel {:server-port 3450
+             :repl        true}
+
+
+  :clean-targets ^{:protect false} ["resources/public/js" "target"]
+
+  :cljsbuild {:builds {:client {:source-paths ["src" "../../src"]
+                                :compiler     {:output-dir "resources/public/js"
+                                               :output-to "resources/public/js/client.js"}}}})
